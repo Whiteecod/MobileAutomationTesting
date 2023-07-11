a73a11534d3b05e6b84147be7e76c134afe9fa2d
kubilayculha <kubilay__culha@hotmail.com>
2023-07-11 18:17:22 +0300

    LoginPage - setUp - LoginFuntionTest - Dropdown - Switch - tearDown

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,4 +1,22 @@
 package Pages;
 
-public class LoginPage {
+import Utility.DriverManage;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends DriverManage {
+     AndroidDriver driver = getAppiumDriver();
+
+     public void LoginPage() {
+         PageFactory.initElements(driver , this);
+     }
+
+    public AppiumBy loginMenu =new AppiumBy.ByAccessibilityId("Login");
+    public AppiumBy usernameInputField =new AppiumBy.ByAccessibilityId("input-email");
+    public AppiumBy passwordInputField =new AppiumBy.ByAccessibilityId("input-password");
+    public AppiumBy loginButton =new AppiumBy.ByAccessibilityId("button-LOGIN");
+    public By successfulLoginMessage = By.id("android:id/message");
+    public By loginOkButton =new By.ById("android:id/button1");
 }
